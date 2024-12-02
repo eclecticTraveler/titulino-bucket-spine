4be900b2077848bf4591ed74e2f216b512fb0be1
Alberto Arellano <are11001@byui.edu>
2024-12-01 19:56:42 -0700

    adjusting bucket settings

--- bucket-scripts/upload.js
+++ bucket-scripts/upload.js
@@ -2,34 +2,34 @@ import { Storage } from '@google-cloud/storage';
 import { join, relative } from 'path';
 import { readdirSync, statSync } from 'fs';
 
-// Initialize Google Cloud Storage with a relative path to the key file
+// Initialize Google Cloud Storage with your credentials
 const storage = new Storage({ keyFilename: './bucket-scripts/certain-upgrade-341601-6db30a4e61b5.json' });
 const bucketName = 'titulino-bucket';
 
-// Function to upload files from a directory (non-recursive approach)
+// Function to upload files from a directory
 async function uploadDirectory(directory) {
   const files = readdirSync(directory);
 
   for (const file of files) {
     const filePath = join(directory, file);
     let destination = relative(join(directory, '..'), filePath); // Maintain folder structure
-     // Normalize the path by replacing backslashes with forward slashes
+    // Normalize the path by replacing backslashes with forward slashes
     destination = destination.replace(/\\/g, '/'); // Replace all backslashes with forward slashes
-
 
     // Check if it's a directory or a file
     if (statSync(filePath).isDirectory()) {
       // If it's a directory, recursively call uploadDirectory
       await uploadDirectory(filePath);
     } else {
-      // If it's a file, upload it with Cache-Control header
-      console.log(`Uploading ${filePath} to ${bucketName}/${destination}...`);
+      // If it's a file, upload it using Google Cloud Storage client
+      console.log(`Uploading ${filePath} to gs://${bucketName}/${destination}...`);
       await storage.bucket(bucketName).upload(filePath, {
         destination,
         metadata: {
           cacheControl: 'no-cache', // Set Cache-Control header
         },
       });
+      console.log(`Upload completed for ${filePath}`);
     }
   }
 
