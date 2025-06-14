8edb345dddc7afd34f28d7c9ce4706e34d47802f
Alberto Arellano <are11001@byui.edu>
2025-06-14 09:18:11 -0600

    test#33

--- bucket-scripts/upload-modded-files.js
+++ bucket-scripts/upload-modded-files.js
@@ -1,12 +1,12 @@
 import { Storage } from '@google-cloud/storage';
-import { join, relative } from 'path';
-import { readdirSync, statSync } from 'fs';
+import { join, relative, resolve } from 'path';
+import { readdirSync, statSync, existsSync } from 'fs';
 
 const isGitHubAction = process.env.GITHUB_ACTIONS === 'true';
 console.log("------>isGitHubAction", isGitHubAction);
-// Initialize Google Cloud Storage with your credentials accordingly for manual run or for github actions run.
+
 const pathToKey = './bucket-scripts/certain-upgrade-341601-6db30a4e61b5.json';
-const useExplicitKey = !isGitHubAction && require('fs').existsSync(pathToKey);
+const useExplicitKey = !isGitHubAction && existsSync(pathToKey);
 
 const storage = useExplicitKey
   ? new Storage({ keyFilename: pathToKey })
@@ -14,7 +14,6 @@ const storage = useExplicitKey
 
 const bucketName = 'titulino-bucket';
 
-// Get remote files metadata
 async function getRemoteFiles() {
   const [files] = await storage.bucket(bucketName).getFiles();
   const remoteFiles = {};
@@ -24,14 +23,12 @@ async function getRemoteFiles() {
   return remoteFiles;
 }
 
-// Function to upload files from a directory if modified
 async function uploadDirectory(directory, remoteFiles) {
   const files = readdirSync(directory);
 
   for (const file of files) {
     const filePath = join(directory, file);
-    let destination = relative(join(directory, '..'), filePath);
-    destination = destination.replace(/\\/g, '/');
+    let destination = relative(join(directory, '..'), filePath).replace(/\\/g, '/');
 
     if (statSync(filePath).isDirectory()) {
       await uploadDirectory(filePath, remoteFiles);
@@ -45,19 +42,17 @@ async function uploadDirectory(directory, remoteFiles) {
           metadata: { cacheControl: 'no-cache' },
         });
         console.log(`Upload completed for ${filePath}`);
-      } else {
-        // console.log(`Skipping ${filePath}, no changes detected.`);        
       }
     }
   }
 }
 
-// Main function
 async function main() {
   try {
     console.log('Fetching remote files metadata...');
     const remoteFiles = await getRemoteFiles();
-    await uploadDirectory('./titulino-bucket', remoteFiles);
+    const uploadDir = resolve('./titulino-bucket');
+    await uploadDirectory(uploadDir, remoteFiles);
   } catch (err) {
     console.error('Error uploading files:', err);
   }
